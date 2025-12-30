b3c50164de3f347e15e9ec11cc36d40ba6725357
Oswald Alessandro <cuman74@gmail.com>
2025-12-30 17:49:16 +0100

    fix: runtime fonts and adapter cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
-    functionPerRoute: false,
   }),
   vite: {
     plugins: [tailwind()],
