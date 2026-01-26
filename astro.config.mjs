50163940d15bc7a148d32195ce0048c7e8e932a8
Oswald Alessandro <cuman74@gmail.com>
2026-01-26 19:22:59 +0100

    feat: implement base layout with comprehensive SEO metadata and configure Astro site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://wedding-landing-ashen.vercel.app",
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
